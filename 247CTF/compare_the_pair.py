29d0b4f6fb106c164d2da5809bd5f2df34080ecc
Caio Dourado <107958780+caioescorcio@users.noreply.github.com>
2024-11-01 12:33:50 -0300

    Update compare_the_pair.py

--- 247CTF/compare_the_pair.py
+++ 247CTF/compare_the_pair.py
@@ -9,9 +9,8 @@ hash = "0e902564435691274142490923013038"
 def hash_md5(salt,word):
     return hashlib.md5((salt + word).encode()).hexdigest()
 
-for i in itertools.count(start=237700000):
-    to_hash = salt + str(i)
-    then_hash = hashlib.md5(to_hash.encode()).hexdigest()
+for i in itertools.count():
+    then_hash = hash_md5(salt, str(i))
     print(f'{to_hash} -> {then_hash}')
     if then_hash[:2] == '0e' and then_hash[2:].isnumeric():
         print("\n\n\nRESULT: ", str(i))
